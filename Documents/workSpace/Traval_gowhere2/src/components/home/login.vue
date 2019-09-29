905086413561da218050a20cffcfac035c639436
xiazhengcheng <xiazc3377@163.com>
2019-09-29 17:00:35 +0800

    修改登录

--- Documents/workSpace/Traval_gowhere2/src/components/home/login.vue
+++ Documents/workSpace/Traval_gowhere2/src/components/home/login.vue
@@ -4,7 +4,7 @@
         <input type="text"  ref="userText" placeholder="请输入用户名">
         </div>
         <div id="pwd">密&nbsp;&nbsp;&nbsp;&nbsp;码：
-        <input type="text"  id="pwdText" placeholder="请输入密码">
+        <input type="text" ref='userPwd' id="pwdText" placeholder="请输入密码">
         </div>
         <button  class="loginBtn" v-on:click="loginClick" type="submit">登 录</button>
         <p> {{ message }} </p>
@@ -13,35 +13,36 @@
 </template>
 <script>
 export default {
-    name: 'login',
-    data () {
-        return {
-            message: '请验证登录信息',
-        }
-    },
-    methods: {
-        loginClick(){
-            this.$router.push('/home');
-            alert(this.$refs.userText.value + '成功！');
-        }
+  name: "login",
+  data() {
+    return {
+      message: "请验证登录信息"
+    };
+  },
+  methods: {
+    loginClick() {
+      if (this.$refs.userText.value === "xiazhengcheng" && this.$refs.userPwd.value === "123") {
+        this.$router.push("/home");
+            alert(this.$refs.userText.value + "成功！");
+      } else {
+            alert('请输入正确的用户名、密码')
+      }
     }
-}
-
+  }
+};
 </script>
 
 <style scoped>
-    p {
-        font-size: 16px;
-        color: red;
-        background-color: antiquewhite;
-
-    }
-    #pwd {
-        margin-top: 30px;
-    }
-    .loginBtn {
-        margin-top: 20px;
-
-    }
+p {
+  font-size: 16px;
+  color: red;
+  background-color: antiquewhite;
+}
+#pwd {
+  margin-top: 30px;
+}
+.loginBtn {
+  margin-top: 20px;
+}
 </style>
 
